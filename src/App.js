9cf28207791e2d9a196d1ce28e4cba0ee0c7d188
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2019-11-13 23:21:27 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Creating react chat-app </h1>
+      <h3 style={{background:"yellow"}}>Chat-me</h3>
+
+      <p>Where i can talk with friends </p>
+    </div>
+  );
+}
+
+export default App;
