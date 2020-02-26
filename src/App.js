b072001c97c5a5e90b5fb91fbf935364f2381386
Nabeel73 <32979012+Nabeel73@users.noreply.github.com>
2020-02-26 22:57:59 +0500

    push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,152 @@
 import React from 'react';
-//import logo from './logo.svg';
+import {Component} from "react";
+import dashboard from "./dashboard";
+import logo from './logo.svg';
+import Message from './message';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Creating react chat-app </h1>
-      <h3 style={{background:"yellow"}}>Chat-me</h3>
-
-      <p>Where i can talk with friends </p>
-    </div>
-  );
+state = {
+  messages: [
+    {
+      text: "This is a test message!",
+      member: {
+        color: "blue",
+        username: "bluemoon"
+      }
+    }
+  ],
+  member: {
+    username: randomName(),
+    color: randomColor()
+  }
+}
+function randomName() {
+  const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  return adjective + noun;
 }
 
-export default App;
+function randomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+class Messages extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Messages
+          messages={this.state.messages}
+          currentMember={this.state.member}
+        />
+      </div>
+    );
+  }
+  renderMessage(message) {
+    const {member, text} = message;
+    const {currentMember} = this.props;
+    const messageFromMe = member.id === currentMember.id;
+    const className = messageFromMe ?
+      "Messages-message currentMember" : "Messages-message";
+    return (
+      <li className={className}>
+        <span
+          className="avatar"
+          style={{backgroundColor: member.clientData.color}}
+        />
+        <div className="Message-content">
+          <div className="username">
+            {member.clientData.username}
+          </div>
+          <div className="text">{text}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+
+export default Messages;
+/*
+class myTodo extends React.Component{
+  render(){
+    return(
+      <div>
+        <ul className="items">
+          <li>Mobile</li>
+          <li>watch</li>
+          <li>Camera</li>
+          <li>laptop</li>
+        </ul>
+      </div>
+    )}
+};
+//addProducts (){
+//this.state.bind{(items)};
+
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+           Add Items->
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoApp;
+
+//export default myTodo;*/
